d62d5bbd440c380178e23378fe711bf9849b3b53
tward1189@gmail.com <tward1189@gmail.com>
2025-03-26 18:18:18 -0400

    update batch file

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 REM Cleanup old nuitka-crash-report.xml
 if exist nuitka-crash-report.xml del nuitka-crash-report.xml
 
-REM Compile the Python script with Nuitka  --remove-output 
-nuitka --onefile --standalone --windows-icon-from-ico=slicer.ico --windows-console-mode=disable --enable-plugin=tk-inter --output-dir=./ --output-filename=slicer.exe --include-data-file="rsc/ffmpeg.exe=rsc/ffmpeg.exe" --msvc=latest slicer.py --show-progress
+REM Compile the Python script with Nuitka
+nuitka --onefile --standalone --windows-icon-from-ico=slicer.ico --remove-output --windows-console-mode=disable --enable-plugin=tk-inter --output-dir=./ --output-filename=slicer.exe --include-data-file="rsc/ffmpeg.exe=rsc/ffmpeg.exe" --msvc=latest slicer.py --show-progress
 pause
